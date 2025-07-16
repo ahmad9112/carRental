f1229526a9181ec2d9b91b3ed2f0e616f7eb6cff
ahmad9112 <saiyadahmad455@gmail.com>
2025-07-16 23:20:25 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
+import Testimonials from '../components/Testimonials'
+import NewsLetter from '../components/NewsLetter'
+function Home() {
+  return (
+    <>
+<Hero/>
+<FeaturedSection/>
+<Banner/>
+<Testimonials/>
+<NewsLetter/>
+    </>
+  )
+}
+
+export default Home
